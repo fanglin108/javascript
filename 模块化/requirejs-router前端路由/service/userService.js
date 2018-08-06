073d852e0071e00c12a40f89d797077540981e8c
FangLin <fanglin@glitzhome.com>
2018-08-06 16:01:44 +0800

    review

--- 模块化/requirejs-router前端路由/service/userService.js
+++ 模块化/requirejs-router前端路由/service/userService.js
@@ -1,22 +1,22 @@
 
 define([], function(){
-	var userList = [
-		{name:'小明', age:20},
-		{name:'小三', age:21}
-	];
+    var userList = [
+        {name:'小明', age:20},
+        {name:'小三', age:21}
+    ];
 
-	return {
-		getList(){
-			return userList;
-		}, 
-		add(name, age){
-			userList.push({name:name, age:age});
-		},
-		update(){
+    return {
+        getList(){
+            return userList;
+        }, 
+        add(name, age){
+            userList.push({name:name, age:age});
+        },
+        update(){
 
-		},
-		del(){
+        },
+        del(){
 
-		}
-	}
+        }
+    }
 })
